2f70b17268343c693f9215462ee698f5f4eb7980
Nicolas Mossmann <nicolas.mossmann@ymail.com>
2023-03-06 19:42:28 -0300

    Redux and react-navigation

--- App.tsx
+++ App.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import {View, Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {store} from './src/store';
+import {Provider} from 'react-redux';
 
 function HomeScreen() {
   return (
@@ -15,11 +17,13 @@ const Stack = createNativeStackNavigator();
 
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
 
