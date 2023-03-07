75ce9b847afbab94f2a7f39e5c6bcdc9245d25e4
Nicolas Mossmann <nicolas.mossmann@ymail.com>
2023-03-07 08:00:52 -0300

    Button updated

--- App/Components/Button/index.tsx
+++ App/Components/Button/index.tsx
@@ -4,7 +4,7 @@ import {TouchableOpacity, Text, StyleSheet} from 'react-native';
 interface ButtonProps {
   title: string;
   onPress(): void;
-  disabled: boolean;
+  disabled?: boolean;
 }
 
 const Button = ({title, onPress, disabled}: ButtonProps) => {
