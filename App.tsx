b3c4e906b9b9ee8bcd22b7cf3c9bb4312c3619a1
Nicolas Mossmann <nicolas.mossmann@ymail.com>
2023-03-07 13:26:07 -0300

    Tab navigation and LoremIpsumScreen added

--- App.tsx
+++ App.tsx
@@ -2,13 +2,16 @@ import * as React from 'react';
 import {View, Text} from 'react-native';
 import {DefaultTheme, NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {store} from './App/Store/store';
 import {Provider} from 'react-redux';
 import HomeScreen from './App/Screens/HomeScreen';
 import LoginScreen from './App/Screens/LoginScreen';
 import SignupScreen from './App/Screens/SignupScreen';
+import LoremIpsumScreen from './App/Screens/LoremIpsumScreen';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const MyTheme = {
   ...DefaultTheme,
@@ -17,6 +20,21 @@ const MyTheme = {
     background: '#FFF',
   },
 };
+
+function Home() {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: '#6a1ad4',
+        tabBarInactiveTintColor: 'gray',
+      }}>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Trade" component={LoremIpsumScreen} />
+      <Tab.Screen name="Portfolio" component={LoremIpsumScreen} />
+    </Tab.Navigator>
+  );
+}
 
 function App() {
   return (
@@ -28,7 +46,7 @@ function App() {
           }}>
           <Stack.Screen name="Login" component={LoginScreen} />
           <Stack.Screen name="Signup" component={SignupScreen} />
-          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Home" component={Home} />
         </Stack.Navigator>
       </NavigationContainer>
     </Provider>
---
+++ App/Screens/LoremIpsumScreen.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Text, StyleSheet, ScrollView} from 'react-native';
+
+const LoginScreen = () => {
+  return (
+    <ScrollView style={styles.container}>
+      <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque a tortor ut dui cursus feugiat non sit amet tellus. Praesent nec urna nisl. In eu eros eget enim pellentesque malesuada et quis mi. In efficitur eget metus a fermentum. Donec sodales eros augue, eu volutpat nunc pellentesque a. Mauris accumsan arcu felis, sed volutpat nulla eleifend quis. Duis placerat lorem quis lacus aliquet consectetur. Sed vehicula volutpat tellus ac faucibus. Praesent vehicula urna tellus, eget volutpat ligula dictum ac. Donec tincidunt justo quis dapibus ultrices. Cras sed lorem in felis pharetra vestibulum. Ut justo ante, mollis sit amet pretium eget, dignissim tristique neque. Sed eros nibh, faucibus in vestibulum tempor, euismod quis urna. Integer nec blandit ex, et molestie nunc. Integer vestibulum lectus vel augue maximus, et aliquet felis euismod. Curabitur non velit metus.</Text>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 20,
+  },
+});
+
+export default LoginScreen;
