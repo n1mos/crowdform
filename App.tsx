62d06216c7ee477946a365c92a5cc69a755a97db
Nicolas Mossmann <nicolas.mossmann@ymail.com>
2023-03-08 07:53:28 -0300

    Tab Navigation updated

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Image} from 'react-native';
 import {DefaultTheme, NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
@@ -25,11 +25,33 @@ const MyTheme = {
 function Home() {
   return (
     <Tab.Navigator
-      screenOptions={{
+      screenOptions={({ route }) => ({
         headerShown: false,
+        tabBarIcon: ({ focused }) => {
+          const icons = {
+            tabHomeSelected: require('./App/Assets/tabHomeSelected.png'),
+            tabHomeUnselected: require('./App/Assets/tabHomeUnselected.png'),
+            tabPortfolioSelected: require('./App/Assets/tabPortfolioSelected.png'),
+            tabPortfolioUnselected: require('./App/Assets/tabPortfolioUnselected.png'),
+            tabTradeSelected: require('./App/Assets/tabTradeSelected.png'),
+            tabTradeUnselected: require('./App/Assets/tabTradeUnselected.png'),
+          };
+
+          let icon;
+
+          if (route.name === 'Home') {
+            icon = focused ? icons.tabHomeSelected : icons.tabHomeUnselected;
+          } else if (route.name === 'Trade') {
+            icon = focused ? icons.tabTradeSelected : icons.tabTradeUnselected;
+          } else if (route.name === 'Portfolio') {
+            icon = focused ? icons.tabPortfolioSelected : icons.tabPortfolioUnselected;
+          }
+
+          return <Image source={icon} />;
+        },
         tabBarActiveTintColor: '#6a1ad4',
         tabBarInactiveTintColor: 'gray',
-      }}>
+      })}>
       <Tab.Screen name="Home" component={HomeScreen} />
       <Tab.Screen name="Trade" component={LoremIpsumScreen} />
       <Tab.Screen name="Portfolio" component={LoremIpsumScreen} />
