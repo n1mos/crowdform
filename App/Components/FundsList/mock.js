6b45e8f13f7ab0eae05fdcf38f1ef8d505cefb24
Nicolas Mossmann <nicolas.mossmann@ymail.com>
2023-03-08 12:32:24 -0300

    Updated mock data

--- App/Components/FundsList/mock.js
+++ App/Components/FundsList/mock.js
@@ -14,9 +14,31 @@ export const WindMock = {
 };
 
 export const SolarMock = {
-  fundDetails: { name: 'Solar' },
+  fundDetails: {
+    name: 'Solar Fund',
+    acronym: 'SFND',
+    price: '$18.23',
+    year: '2022',
+    AUM: '$430.88m',
+    issueDate: '18/04/2022',
+    vintageRange: '2019 - 2022',
+    TER: '0.15%',
+    priceAtClose: '$17.68',
+    priceAtOpen: '$17.74',
+  },
 };
 
 export const NatureMock = {
-  fundDetails: { name: 'Nature' },
+  fundDetails: {
+    name: 'Nature Fund',
+    acronym: 'NFND',
+    price: '$18.23',
+    year: '2022',
+    AUM: '$430.88m',
+    issueDate: '18/04/2022',
+    vintageRange: '2019 - 2022',
+    TER: '0.15%',
+    priceAtClose: '$17.68',
+    priceAtOpen: '$17.74',
+  },
 };
