a9e3103befe1eb939f1d760fc9fd27d8f3cd3c04
Nicolas Mossmann <nicolas.mossmann@ymail.com>
2023-03-08 07:17:58 -0300

    TipsBanner updated

--- App/Components/TipsBanner/index.tsx
+++ App/Components/TipsBanner/index.tsx
@@ -1,22 +1,35 @@
 import React from 'react';
-import {Text, StyleSheet, View} from 'react-native';
+import {Text, StyleSheet, View, Image} from 'react-native';
 
 const Button = () => {
+  const iconTips = require('../../Assets/tips.png');
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>{`Learn more about\ncarbon credits`}</Text>
-      <Text style={styles.text}>Check out our top tips!</Text>
+      <View>
+        <Text style={styles.title}>{`Learn more about\ncarbon credits`}</Text>
+        <Text style={styles.text}>Check out our top tips!</Text>
+      </View>
+      <View style={styles.iconContainer}>
+        <Image source={iconTips} style={styles.icon} />
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    flexDirection: 'row',
     backgroundColor: '#6a1ad4',
     paddingVertical: 20,
     paddingHorizontal: 20,
     marginTop: 40,
     borderRadius: 10,
+  },
+  iconContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'flex-end',
   },
   title: {
     color: '#FFF',
@@ -28,6 +41,9 @@ const styles = StyleSheet.create({
     fontSize: 14,
     marginTop: 20,
   },
+  icon: {
+    
+  }
 });
 
 export default Button;
